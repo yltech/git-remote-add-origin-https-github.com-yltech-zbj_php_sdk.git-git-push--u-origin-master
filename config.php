75eb6004b4a124f2404da85212f5fa8998423f4b
Ansion <1572309495@qq.com>
2018-03-13 18:05:45 +0800

    Update config.php

--- config.php
+++ config.php
@@ -6,7 +6,7 @@
  * @since  2018-3-12
  */
 define("APP_KEY", "20180308165031Kdjd3s");
-define("APP_SECRET", "6B8B11E354F99EA4DA796D59B2174BB7");
-define("URL_CALLBACK", "https://www.yltech.cc/zbj/supper_order/callback");
-define("URL_CALLMSG", "https://www.yltech.cc/zbj/supper_order/callmsg");
+define("APP_SECRET", "6B8B11E354F99EA4DA796D59B2171127");
+define("URL_CALLBACK", "https://www.yltech.cc/zbj/supper_order/type/callback");
+define("URL_CALLMSG", "https://www.yltech.cc/zbj/supper_order/type/callmsg");
  ?>
